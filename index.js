da78a9dbb93580f3404ee18fae5c9b959f4c242d
qquodd <ura.kvochkin@gmail.com>
2024-01-16 20:43:39 +0300

    last_update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Подключение модуля Express
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
@@ -9,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); //Для принятия данных из формы. Обязательно!!!
 app.use(bodyParser.json());//Для принятия данных из формы. Обязательно!!!
 
-
+// Подключение и использование различных маршрутов (routes):
 let index = require('./routes/index');
 app.use('/', index);
 
@@ -21,5 +22,5 @@ app.use('/article', article);
 let admin = require('./routes/admin');
 app.use('/admin', admin);
 
-
+// Запуск сервера на порту 3000
 app.listen(3000, 'localhost');
