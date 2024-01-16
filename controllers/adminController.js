d96859aeead6e64387fbe6d4100ee8696fe0f217
qquodd <ura.kvochkin@gmail.com>
2024-01-16 19:20:52 +0300

    controllers

---
+++ controllers/adminController.js
@@ -0,0 +1,28 @@
+const Models = require("../moduls/adminModul.js");
+
+exports.getArticleAll = async function(req, res){
+    let m=await Models.getAll(); 
+    res.render('adminArticles', {data:m});
+};
+
+exports.getArticle = async function(req, res){ 
+    let m=await Models.getOne(req.params.idArticle); 
+    res.render('adminArticleShow', {data:m});
+};
+exports.addArticle = async function(req, res){
+    await Models.addOne(req.body); 
+    let m=await Models.getAll(); 
+    res.render('adminArticles', {data:m});
+};
+exports.editArticle = async function(req, res){
+    await Models.editOne(req.body); 
+    let m=await Models.getOne(req.body.idArticle); 
+    res.render('adminArticleShow', {data:m});
+};
+exports.deleteArticle = async function(req, res){ 
+    // Предполагается, что idArticle передается в запросе
+    const idArticle = req.body.idArticle;
+    await Models.deleteOne(idArticle); 
+    let m=await Models.getAll();
+    res.render('adminArticles', {data:m});
+};
